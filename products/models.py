124791d99bb1113505c81da0f56ac9eb3224bcb2
Patrick Galvin <patjoegalvin@gmail.com>
2024-10-15 16:30:42 +0000

    Remove Extra Models for Description

--- products/models.py
+++ products/models.py
@@ -20,8 +20,6 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.TextField(), models.TextField()
-    description = models.TextField()
     description = models.TextField() 
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
