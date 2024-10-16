59411c0cdd59f840960176b9aa1d820e161b6d02
Patrick Galvin <patjoegalvin@gmail.com>
2024-10-16 10:47:45 +0000

    Add: Custom products through admin site

--- useire/settings.py
+++ useire/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'bag.contexts.bag_content',
+                'bag.contexts.bag_contents',
             ],
         },
     },
