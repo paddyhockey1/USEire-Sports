bdae391077c398c754a5714bbbc98568e5e0f3c8
Patrick Galvin <patjoegalvin@gmail.com>
2024-10-21 08:42:03 +0000

    Add: Initial Stripe code

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QCGuODyLxdlHT8TwyywDVVBc6qtaFM7k4UNOFcXKxX2QwARnfswIVNOtPe1FQwTMWJcYOaGpyBpKJtUNxP8g3zq00aToXunsW',
+        'client_secret': 'test_client_secret'
     }
 
     return render(request, template, context)
