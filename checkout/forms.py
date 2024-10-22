395a2ff2977b312c140cfd1a9e041baa5e482f7f
Patrick Galvin <patjoegalvin@gmail.com>
2024-10-22 08:28:56 +0000

    Add: Street address 3 to cripsy forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,7 +6,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',
+                  'street_address1', 'street_address2', 'street_address3',
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
@@ -25,6 +25,7 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
+            'street_address3': 'Street Address 3',
             'county': 'County',
         }
 
