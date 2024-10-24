9f1fbaf4f1ddd50036fc3a85a4712978251ea9e3
Patrick Galvin <patjoegalvin@gmail.com>
2024-10-24 14:06:13 +0000

    Recreate missing template folders

--- useire/urls.py
+++ useire/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('discounts/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
