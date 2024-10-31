75b96bd9641b3f7072a5ab1f70431ecc058e496b
Patrick Galvin <patjoegalvin@gmail.com>
2024-10-31 10:59:15 +0000

    Add: email integration

--- useire/settings.py
+++ useire/settings.py
@@ -124,7 +124,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://useire-sports-b029765775db.herokuapp.com/',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -233,4 +233,15 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', ' ')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', ' ')
 STRIPE_WH_KEY = os.environ.get('STRIPE_WH_KEY', ' ')
-DEFAULT_FROM_EMAIL = 'useiresports@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'useiresports@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
