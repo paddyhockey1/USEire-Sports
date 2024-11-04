d89ea71d61051050c1dd96989b755eae1a0eab54
Patrick Galvin <patjoegalvin@gmail.com>
2024-11-04 12:04:26 +0000

    Add: Final AWS Steps

--- useire/settings.py
+++ useire/settings.py
@@ -200,7 +200,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'paddyhockey1-useire-sports'
+    AWS_STORAGE_BUCKET_NAME = 'useire-sports-bucket'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
